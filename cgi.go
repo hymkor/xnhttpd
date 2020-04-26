72367597df6a6c081c1211b4dc96eb6483844311
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-04-27 00:56:17 +0900

    Split code for calling CGI to another function

---
+++ cgi.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var rxPortNo = regexp.MustCompile(`:\d+$`)
+
+func callCgi(interpreter, script string,
+	w http.ResponseWriter,
+	req *http.Request,
+	logger, cgierr io.Writer) error {
+
+	cmd := exec.Command(interpreter, script)
+	toCgi, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	var cookie strings.Builder
+	for _, c := range req.Cookies() {
+		if cookie.Len() > 0 {
+			cookie.WriteString("; ")
+		}
+		cookie.WriteString(c.String())
+	}
+
+	env := []string{
+		"QUERY_STRING=" + req.URL.RawQuery,
+		fmt.Sprintf("CONTENT_LENGTH=%d", req.ContentLength),
+		"REQUEST_METHOD=" + strings.ToUpper(req.Method),
+		"HTTP_COOKIE=" + cookie.String(),
+		"HTTP_USER_AGENT=" + req.UserAgent(),
+		"SCRIPT_NAME=" + req.URL.Path,
+		"REMOTE_ADDR=" + rxPortNo.ReplaceAllString(req.RemoteAddr, ""),
+	}
+	cmd.Env = append(env, os.Environ()...)
+
+	fromCgi, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	go func() {
+		br := bufio.NewReader(fromCgi)
+		defer fromCgi.Close()
+		for {
+			line, err := br.ReadString('\n')
+			if err != nil {
+				break
+			}
+			line = strings.TrimSpace(line)
+			f := strings.SplitN(line, ": ", 2)
+			if len(f) < 2 {
+				w.WriteHeader(http.StatusOK)
+				io.Copy(w, br)
+				return
+			}
+			w.Header().Add(f[0], f[1])
+		}
+	}()
+
+	cmd.Stderr = cgierr
+	cmd.Start()
+	if logger != nil {
+		io.WriteString(logger, "Start\n")
+	}
+	io.Copy(toCgi, req.Body)
+	toCgi.Close()
+	req.Body.Close()
+	cmd.Wait()
+	if logger != nil {
+		io.WriteString(logger, "Done\n")
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,11 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
-	"regexp"
-	"strings"
 	"time"
 )
 
@@ -35,8 +31,6 @@ type Handler struct {
 	notFound http.Handler
 }
 
-var rxPortNo = regexp.MustCompile(`:\d+$`)
-
 func (this *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	suffix := path.Ext(req.URL.Path)
 	interpreter, ok := this.Config.Handler[suffix]
@@ -52,64 +46,9 @@ func (this *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 	interpreter = filepath.FromSlash(interpreter)
 	log.Printf("\"%s\" \"%s\"\n", interpreter, script)
-	cmd := exec.Command(interpreter, script)
-	toCgi, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
+	if err := callCgi(interpreter, script, w, req, os.Stderr, os.Stderr); err != nil {
+		log.Fatal(err.Error())
 	}
-
-	var cookie strings.Builder
-	for _, c := range req.Cookies() {
-		if cookie.Len() > 0 {
-			cookie.WriteString("; ")
-		}
-		cookie.WriteString(c.String())
-	}
-
-	env := []string{
-		"QUERY_STRING=" + req.URL.RawQuery,
-		fmt.Sprintf("CONTENT_LENGTH=%d", req.ContentLength),
-		"REQUEST_METHOD=" + strings.ToUpper(req.Method),
-		"HTTP_COOKIE=" + cookie.String(),
-		"HTTP_USER_AGENT=" + req.UserAgent(),
-		"SCRIPT_NAME=" + req.URL.Path,
-		"REMOTE_ADDR=" + rxPortNo.ReplaceAllString(req.RemoteAddr, ""),
-	}
-	cmd.Env = append(env, os.Environ()...)
-
-	fromCgi, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	go func() {
-		br := bufio.NewReader(fromCgi)
-		defer fromCgi.Close()
-		for {
-			line, err := br.ReadString('\n')
-			if err != nil {
-				break
-			}
-			line = strings.TrimSpace(line)
-			f := strings.SplitN(line, ": ", 2)
-			if len(f) < 2 {
-				w.WriteHeader(http.StatusOK)
-				io.Copy(w, br)
-				return
-			}
-			w.Header().Add(f[0], f[1])
-		}
-	}()
-
-	cmd.Stderr = os.Stderr
-	cmd.Start()
-	log.Println("Start")
-	// io.Copy(toCgi, io.TeeReader(req.Body, os.Stderr))
-	// fmt.Fprintln(os.Stderr)
-	io.Copy(toCgi, req.Body)
-	toCgi.Close()
-	req.Body.Close()
-	cmd.Wait()
-	log.Println("Done")
 }
 
 func mains(args []string) error {
